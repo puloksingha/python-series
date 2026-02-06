9a757afc8c665b52ddc3427b633ee5a05c271f7a
pulok singha <puloksingha2018@gmail.com>
2026-02-06 23:01:29 +0530

    day-2

---
+++ Day-02/index.py
@@ -0,0 +1,20 @@
+# datatype in python
+# int, float, str, bool 
+# integer numbers
+a = 10
+print(a)
+# float numbers
+b = 3.14
+print(b)
+# string
+c = "Hello, World!" 
+print(c)
+# boolean
+d = True
+print(d)
+# type of data
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
